50e6fbf26792fd5472d0e3e22f168ff8bd5e730a
mbsomeday <137542994@qq.com>
2024-08-15 17:14:39 +0100

    a

---
+++ exe_functions/ds_cls.py
@@ -0,0 +1,61 @@
+import sys, os, argparse
+
+# 将上级目录加入 sys.path， 防止命令行运行时找不到包
+curPath = os.path.abspath(os.path.dirname(__file__))
+root_path = os.path.split(curPath)[0]
+sys.path.append(root_path)
+
+from testing.test import datasetCls_all
+from cv_models import VARS_LOCAL, VARS_CLOUD, DEVICE
+
+
+def get_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ds_name_list', nargs='+', default=['D1', 'D2', 'D3', 'D4'])
+    parser.add_argument('--ds_label_list', nargs='+', type=int, default=[0, 1, 2, 3])
+    parser.add_argument('--txt_name', type=str, default='test.txt')
+    parser.add_argument('--batch_size', type=int, default=4)
+    parser.add_argument('--var_opt', type=str, default='CLOUD')
+
+    args = parser.parse_args()
+
+    return args
+
+
+opts = get_opt()
+
+
+ds_name_list = opts.ds_name_list
+ds_label_list = opts.ds_label_list
+txt_name = opts.txt_name
+var_opt = opts.var_opt
+
+if var_opt == 'CLOUD':
+    runOn = VARS_CLOUD
+else:
+    runOn = VARS_LOCAL
+
+opt_dict = {
+    'batch_size': opts.batch_size
+}
+
+datasetCls_all(runOn, ds_name_list, ds_label_list, txt_name, opt_dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- testing/test.py
+++ testing/test.py
@@ -17,13 +17,13 @@ from cv_models import VARS_LOCAL, VARS_CLOUD, DEVICE
 from dataset.dataset import dsCls_Dataset
 
 
-def datasetCls_all(var_opt, ds_name_list, ds_label_list, txt_name, opt_dict):
+def datasetCls_all(runOn, ds_name_list, ds_label_list, txt_name, opt_dict):
     '''
         对全部数据进行数据集分类
     '''
 
     # 模型准备
-    ds_weights_path = var_opt['dsCls_weights']
+    ds_weights_path = runOn['dsCls_weights']
     model = vgg16_bn(4)
     checkpoints = torch.load(ds_weights_path, map_location=torch.device(DEVICE))
     model.load_state_dict(checkpoints['model_state_dict'])
@@ -33,7 +33,7 @@ def datasetCls_all(var_opt, ds_name_list, ds_label_list, txt_name, opt_dict):
     batch_size = opt_dict['batch_size']
 
     # 数据准备
-    test_dataset = dsCls_Dataset(var_opt, ds_name_list, ds_label_list, txt_name)
+    test_dataset = dsCls_Dataset(runOn, ds_name_list, ds_label_list, txt_name)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
     # 开始检验
@@ -62,32 +62,6 @@ def datasetCls_all(var_opt, ds_name_list, ds_label_list, txt_name, opt_dict):
     print(f'Dataset Classification accuracy on all dataset: {test_accuracy:.4f}, detail:{correct_num}/{len(test_dataset)}')
 
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--ds_name_list', nargs='+', default=['D1', 'D2', 'D3', 'D4'])
-    parser.add_argument('--ds_label_list', nargs='+', type=int, default=[0, 1, 2, 3])
-    parser.add_argument('--txt_name', type=str, default='test.txt')
-    parser.add_argument('--batch_size', type=int, default=4)
-    parser.add_argument('--var_opt', type=str, default='CLOUD')
-
-    args = parser.parse_args()
-
-    ds_name_list = args.ds_name_list
-    ds_label_list = args.ds_label_list
-    txt_name = args.txt_name
-    var_opt = args.var_opt
-
-    if var_opt == 'CLOUD':
-        var_opt = VARS_CLOUD
-    else:
-        var_opt = VARS_LOCAL
-
-    opt_dict = {
-        'batch_size': args.batch_size
-    }
-
-    datasetCls_all(var_opt, ds_name_list, ds_label_list, txt_name, opt_dict)
 
 
 
