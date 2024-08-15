1a8b43a3f8b19fb3742f688be64081efe9b978fb
mbsomeday <137542994@qq.com>
2024-08-15 15:46:59 +0100

    vgg

---
+++ cv_models/__init__.py
@@ -0,0 +1,25 @@
+import torch
+import os
+
+
+VARS_LOCAL = {
+    'dsCls_weights': r'D:\my_phd\Model_Weights\Stage4\Baseline\vgg16bn-dsCls-029-0.9777.pth',
+
+}
+
+
+VARS_CLOUD = {
+    'dsCls_weights': r'D:\my_phd\Model_Weights\Stage4\Baseline\vgg16bn-dsCls-029-0.9777.pth',
+}
+
+
+
+
+
+
+
+
+
+
+
+
