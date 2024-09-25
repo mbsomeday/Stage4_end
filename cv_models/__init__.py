9fc6d3f27a027588f2f92ad0980bd664d2423983
mbsomeday <137542994@qq.com>
2024-09-25 10:59:32 +0100

    update

--- cv_models/__init__.py
+++ cv_models/__init__.py
@@ -43,6 +43,10 @@ VARS_CLOUD = {
     'D3toD4': r'/kaggle/input/stage4-d3tod4-dataset-stable',
     'D4toD4': r'/kaggle/input/stage4-d4tod4-stable',
 
+    'G4D1': r'/kaggle/input/stage4-g4d123/Stage4_G4D1_Stable',
+    'G4D2': r'/kaggle/input/stage4-g4d123/Stage4_G4D2_Stable',
+    'G4D3': r'/kaggle/input/stage4-g4d123/Stage4_G4D3_Stable',
+
     'dsCls_weights': r'/kaggle/input/stage4-dscls-weights/vgg16bn-dsCls-029-0.9777.pth',
 
     'weights': {
